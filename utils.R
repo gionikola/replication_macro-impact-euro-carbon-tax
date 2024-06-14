7c2554c6fdb974a01edfc81b501d8a5516768c04
Giorgi Nikolaishvili <gnikolai@uoregon.edu>
2024-06-13 18:18:31 -0700

    Create LP IRF scaling function

--- utils.R
+++ utils.R
@@ -9,3 +9,15 @@ pacman::p_load(tidyr,
                rio,
                lpirfs,
                panelvar) 
+
+# `scale_irfs()`
+# Scales input `lpirfs_lin_panel_obj` IRF
+# point estimates and CIs by a scaling factor.
+scale_irfs <- function(irf_obj, scale_factor){
+  
+  irf_obj[1][[1]] <- irf_obj[1][[1]] * scale_factor
+  irf_obj[2][[1]] <- irf_obj[2][[1]] * scale_factor
+  irf_obj[3][[1]] <- irf_obj[3][[1]] * scale_factor
+  
+  return(irf_obj)
+}
