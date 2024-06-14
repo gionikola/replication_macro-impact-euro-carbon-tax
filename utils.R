d97ff40bc9d0c7d3db677467812ca6dd52bc7db7
Giorgi Nikolaishvili <gnikolai@uoregon.edu>
2024-06-13 17:19:02 -0700

    Add script that loads utils

---
+++ utils.R
@@ -0,0 +1,11 @@
+# Load necessary packages 
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyr,
+               dplyr, 
+               stats,
+               vars, 
+               ggplot2,
+               readr,
+               rio,
+               lpirfs,
+               panelvar) 
